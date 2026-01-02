3e0f243c92b52b886c4e60b4b22e2fa74992e417
vaibhhaav <vaibhavgupta2021@gmail.com>
2026-01-02 22:15:16 +0530

    Date: 02/01/2026 - Add solution for sorting 0s, 1s, and 2s in array
    Implements the Dutch National Flag algorithm in the Solution class to sort an array containing only 0s, 1s, and 2s in a single pass.

---
+++ Sort0s1sand2s.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sort012(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+        
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+    
+    public void swap (int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
