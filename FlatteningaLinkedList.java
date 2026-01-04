0e4b4c09a385b90be777ba795e75d1e72c026e56
vaibhhaav <vaibhavgupta2021@gmail.com>
2026-01-04 22:49:19 +0530

    Date: 03/01/2026 - Add solution for flattening a linked list
    Implements a Solution class with methods to flatten a multi-level linked list using recursion and merging. Includes a helper merge function to combine two sorted linked lists via their 'bottom' pointers.

---
+++ FlatteningaLinkedList.java
@@ -0,0 +1,45 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node bottom;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        bottom = null;
+    }
+}
+*/
+class Solution {
+    public Node flatten(Node root) {
+        if (root == null || root.next == null) {
+            return root;
+        }
+        
+        root.next = flatten(root.next);
+        
+        root = merge(root, root.next);
+        
+        return root;
+    }
+    
+    private Node merge(Node a, Node b) {
+        Node dummy = new Node(-1);
+        Node tail = dummy;
+        
+        while(a != null && b != null) {
+            if (a.data <= b.data) {
+                tail.bottom = a;
+                a = a.bottom;
+            } else {
+                tail.bottom = b;
+                b = b.bottom;
+            }
+            tail = tail.bottom;
+        }
+        
+        tail.bottom = (a != null) ? a : b;
+        return dummy.bottom;
+    }
+}
