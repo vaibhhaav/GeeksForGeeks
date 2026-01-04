cc7b8091a0d1edb27b1b84f678263a682336fd98
vaibhhaav <vaibhavgupta2021@gmail.com>
2026-01-04 22:55:57 +0530

    Date: 02/01/2026 - Add solution to move all zeros to end of array
    Introduces MoveAllZeroestoEnd.java with a method to shift all zeros in an array to the end while maintaining the order of non-zero elements.

---
+++ MoveAllZeroestoEnd.java
@@ -0,0 +1,14 @@
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int count = 0;
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+                count++;
+            }
+        }
+    }
+}
