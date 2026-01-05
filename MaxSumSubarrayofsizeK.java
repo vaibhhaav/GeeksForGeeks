44103b8b2401ec06e41a9cf36914f4a69bb0865d
vaibhhaav <vaibhavgupta2021@gmail.com>
2026-01-05 23:11:30 +0530

    Date: 05/10/2026 - Add max subarray sum of size K solution
    Implements a sliding window algorithm to find the maximum sum of any subarray of size K in an integer array.

---
+++ MaxSumSubarrayofsizeK.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        int n = arr.length;
+        if (k > n) return -1;
+
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        for (int i = k; i < n; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+
+    }
+}
