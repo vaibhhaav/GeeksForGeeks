5eeea22e6db657a21f57e8589babc99fa4704c78
vaibhhaav <vaibhavgupta2021@gmail.com>
2026-01-07 21:30:22 +0530

    Date: 07/01/2026 - Add solution for stock buy and sell problem
    Implements the maximumProfit method to calculate the maximum profit from multiple stock transactions. The algorithm sums all positive price differences between consecutive days.

---
+++ StockBuyandSellMultipleTransactionAllowed.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+class Solution {
+    public int maximumProfit(int prices[]) {
+        int res = 0;
+        
+        for (int i=1; i<prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                res += prices[i] - prices[i-1];
+            }
+        }
+        return res;
+    }
+}
