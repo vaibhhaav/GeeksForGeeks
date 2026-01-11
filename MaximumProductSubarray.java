ccd7d5678d0a84be3830c627eb7ef9f0834516b1
vaibhhaav <vaibhavgupta2021@gmail.com>
2026-01-11 23:23:01 +0530

    Date: 11/01/2026 - Add Maximum Product Subarray solution
    Implements a function to find the maximum product of a contiguous subarray in an integer array using dynamic programming.

---
+++ MaximumProductSubarray.java
@@ -0,0 +1,25 @@
+class Solution {
+    static int max(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+    
+    static int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+    
+    int maxProduct(int[] arr) {
+        int n = arr.length;
+        int currMax = arr[0];
+        int currMin = arr[0];
+        int maxProd = arr[0];
+        
+        for(int i=1; i<n; i++) {
+            int temp = max(arr[i], arr[i]*currMax, arr[i]*currMin);
+            currMin = min(arr[i], arr[i]*currMax, arr[i]*currMin);
+            currMax = temp;
+            maxProd = Math.max(maxProd, currMax);
+        }
+        
+        return maxProd;
+    }
+}
