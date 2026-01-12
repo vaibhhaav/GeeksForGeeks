e8e52c64fc3d3b0b4addc50dd1c9831ab74ebc9f
vaibhhaav <vaibhavgupta2021@gmail.com>
2026-01-12 20:19:55 +0530

    Date: 12/01/2026 - Add sliding window maximum for k-sized subarrays
    Implements a function to find the maximum in every contiguous subarray of size k using a deque for efficient computation.

---
+++ KSizedSubarrayMaximum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() <= i-k) {
+                dq.pollFirst();
+            }
+            
+            while (!dq.isEmpty() && arr[dq.peekLast()] <= arr[i]) {
+                dq.pollLast();
+            }
+            
+            dq.offerLast(i);
+            
+            if (i >= k-1) {
+                result.add(arr[dq.peekFirst()]);
+            }
+        }
+        return result;
+    }
+}
